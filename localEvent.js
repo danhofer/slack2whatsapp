7696593c074efe7607f762864f0db470b1283455
Dan Hofer <danhofer@gmail.com>
2019-04-15 23:28:33 -0400

    edit local test

--- localEvent.js
+++ localEvent.js
@@ -1,10 +1,12 @@
 const user = process.env.SLACK_EVENT_USER
 const channel = process.env.SLACK_EVENT_CHANNEL
 
+const text = 'TEST FROM LOCAL'
+
 module.exports = function() {
     return {
         event: {
-            text: 'one more test',
+            text,
             user,
             channel,
         },
